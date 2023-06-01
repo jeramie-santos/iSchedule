a25887444a12580826fa5a10a578049c91142362
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-06-01 19:00:23 +0800

    Initial code made prior creating Git repo

---
+++ js/burger.js
@@ -0,0 +1,33 @@
+const menuBtn = document.querySelector('.menu-btn');
+const links = document.querySelector('.header__nav-mobile--links');
+const burger = document.querySelector('.menu-btn__burger');
+let menuOpen = false;
+
+burger.addEventListener('click', () => {
+
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    links.style = "display:flex";
+    menuOpen = true;
+  }
+  else{
+    menuBtn.classList.remove('open');
+    links.style.display = "none";
+    menuOpen = false;
+  }
+
+}); 
+
+document.onclick = (event) => {
+    if((event.target.className != "menu-btn__burger" && event.target.className != "dropdown-item")){
+      menuBtn.classList.remove('open');
+      links.style.display = "none";
+      menuOpen = false;
+      console.log(event.target.className);
+  }
+}
+
+
+  
+
+  
