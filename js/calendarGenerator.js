08be81cb1a17e49d91e7473be1ac17be162f076b
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-06-28 18:45:37 +0800

    Finish layout for third part of multipart form

---
+++ js/calendarGenerator.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.calendar-container');
+
+
+
+// const d = new Date('October 17, 2001');
+
+// alert(d.getDay());
+
+// alert('');
+// let box = document.createElement('div');
+// box.classList.add('box');
+
+// container.appendChild(box);
