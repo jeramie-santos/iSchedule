607aaedcc35f8032ef29bb770c27fabb32fee43e
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-09-18 19:00:22 +0800

    Fix session expiry in feedback

--- php/checkHasSubmitFb.php
+++ php/checkHasSubmitFb.php
@@ -1,22 +1,7 @@
 <?php
-    // Set session cookie attributes
-    session_set_cookie_params([
-        'lifetime' => 0,  // Session expires when the browser is closed
-        'path' => '/',
-        'domain' => '',
-        'secure' => true,  // Only send cookies over HTTPS
-        'httponly' => true,  // Prevent JavaScript access to the cookie
-        'samesite' => 'Strict',  // Improve CSRF protection
-    ]);
-    ini_set('session.gc_maxlifetime', 86400);
-    session_set_cookie_params(86400);
-
-
     session_start();
 
     if(isset($_SESSION['fbSubmittedToday'])){
-        // required to para mapagana yung expiration nung session
-        session_regenerate_id(true);
         echo 1;
     }
     else{
--- php/postFeedback.php
+++ php/postFeedback.php
@@ -1,6 +1,5 @@
 <?php
     require 'connect.php';
-    session_start();
 
     $requestPayload = file_get_contents("php://input");
     $object = json_decode($requestPayload);
@@ -10,7 +9,22 @@
     $query = "INSERT INTO `feedback`(`rate`, `feedbackContent`) VALUES ('$object->rate','$convertedSpeChar')";
 
     if(mysqli_query($conn, $query)){
+            // Set session cookie attributes
+        session_set_cookie_params([
+            'lifetime' => 0,  // Session expires when the browser is closed
+            'path' => '/',
+            'domain' => '',
+            'secure' => true,  // Only send cookies over HTTPS
+            'httponly' => true,  // Prevent JavaScript access to the cookie
+            'samesite' => 'Strict',  // Improve CSRF protection
+        ]);
+        ini_set('session.gc_maxlifetime', 86400);
+        session_set_cookie_params(86400);
+
+        session_start();
         $_SESSION['fbSubmittedToday'] = true;
+        // required to para mapagana yung expiration nung session
+        session_regenerate_id(true);
         echo 1;
     }
 ?>
